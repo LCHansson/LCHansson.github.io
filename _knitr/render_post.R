646e855ef3cdfdacad42bdc22f3ef54c541e5859
LCHansson <love.hansson@gmail.com>
2014-11-02 22:03:48 +0100

    Update Rmd renderer code

--- _knitr/render_post.R
+++ _knitr/render_post.R
@@ -7,7 +7,9 @@ KnitPost <- function(input, base.url = "/") {
     fig.path <- paste0("../fig/", sub(".Rmd$", "", basename(input)), "/")
     opts_chunk$set(fig.path = fig.path)
     opts_chunk$set(fig.cap = "center")
-    render_jekyll()
+    opts_chunk$set(collapse=TRUE)
+    opts_chunk$set(comment=">")
+    render_jekyll(highlight = 'pygments')
     print(paste0("../_posts/", sub(".Rmd$", "", basename(input)), ".md"))
     knit(input, output = paste0("../_posts/", sub(".Rmd$", "", basename(input)), ".md"), envir = parent.frame())
 }
